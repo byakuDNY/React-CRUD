29d370aa1f712b5354ed97a376a3ab631fe2a1f9
Roberto Luo <r13luo.gmail.com>
2024-03-08 17:46:26 -0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import {Routes, Route, Link} from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import EditPage from "./pages/EditPage";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -13,21 +13,22 @@ const App = () => {
     <div>
       <nav className="bg-gray-800">
         <div className="container mx-auto p-2">
-          <Link to="/"><h2 className="text-white text-2xl font-bold">React CRUD</h2></Link>
+          <Link to="/">
+            <h2 className="text-white text-2xl font-bold">React CRUD</h2>
+          </Link>
         </div>
-      </nav>  
+      </nav>
 
-      <div className="container mx-auto p-2 h-full">     
+      <div className="container mx-auto p-2 h-full">
         <Routes>
-          <Route index element={<HomePage/>}></Route>
-          <Route path="/create" element={<CreatePage/>}></Route>
-          <Route path="/edit/:id" element={<EditPage/>}></Route>
+          <Route index element={<HomePage />}></Route>
+          <Route path="/create" element={<CreatePage />}></Route>
+          <Route path="/edit/:id" element={<EditPage />}></Route>
         </Routes>
       </div>
-      <ToastContainer/>
-
+      <ToastContainer />
     </div>
   );
-}
+};
 
 export default App;
